8e1afdf75cddf724c63dda2886017465ee6e703a
unknown <karnamvarun7702@gmail.com>
2025-05-08 23:54:28 +0530

    code changes by varun

---
+++ git.py
@@ -0,0 +1,30 @@
+ashwi = input("couple : ")
+meet = input("time : ")
+i="sreekanth"
+
+while i<="sreekanth neweeee":
+    print("Triveniieeeeiii")
+
+
+
+
+if ashwi == "varun":
+    print("i love you")
+    print("i love you kanna")
+
+elif ashwi == "triveni" or meet == "6:30":
+     print("ilove you trivu")
+
+else :
+    print("I hate you")
+    print("I hate you kanna")
+
+ 
+
+
+
+
+
+
+
+
